6bf8c132c7fcb72e2f6a979946e172963906c8a5
Jeremypoh05 <jeremypoh0205@gmail.com>
2022-01-07 00:42:13 +0800

    Set up the login page and dashboard page

--- BlogProject/app/Http/Controllers/AdminController.php
+++ BlogProject/app/Http/Controllers/AdminController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin; 
 
 class AdminController extends Controller
 {
     // Login view
-    function login(){
+    public function login(){
         return view('backend.login'); //since the login file is inside the backend folder
+    }
+
+    // Submit login
+    public function submitLogin(Request $request){
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $userCheck=Admin::where(['username'=>$request->username,'password'=>$request->password])->count();
+    	if($userCheck>0){
+            $adminData=Admin::where(['username'=>$request->username,'password'=>$request->password])->first();
+    		return redirect('admin/dashboard');
+    	}else{
+    		return redirect('admin/login')->with('Invalid username or password!');
+    	}
+    }
+
+    // Dashboard
+    public function dashboard(){
+    	return view('backend.dashboard');
     }
 }
--- BlogProject/resources/views/backend/login.blade.php
+++ BlogProject/resources/views/backend/login.blade.php
@@ -11,11 +11,14 @@
 
     <title>Admin Login</title>
 
-    <!-- Bootstrap core CSS-->
-    <link href="{{asset('backend')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <!-- Bootstrap core CSS-->
+     <link href="{{asset('backend')}}/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('backend')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="{{asset('backend')}}/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{asset('backend')}}/css/sb-admin.css" rel="stylesheet">
