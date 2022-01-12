15995fda66f9cc26bfc30e60ef2b90232f21087c
Jeremypoh05 <jeremypoh0205@gmail.com>
2022-01-13 00:22:23 +0800

    set post view and status default value

--- BlogProject/database/migrations/2022_01_08_151317_views_to_posts.php
+++ BlogProject/database/migrations/2022_01_08_151317_views_to_posts.php
@@ -15,7 +15,7 @@ class ViewsToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->integer('views')->default(1);
+            $table->integer('views')->default('0');
         });
     }
 
