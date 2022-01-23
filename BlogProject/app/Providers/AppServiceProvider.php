b39b5d657ff1a8cf98792a103b0badf6fa10daf6
Jeremypoh05 <jeremypoh0205@gmail.com>
2022-01-23 22:44:31 +0800

    Delete unsue image

--- BlogProject/app/Providers/AppServiceProvider.php
+++ BlogProject/app/Providers/AppServiceProvider.php
@@ -25,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
      public function boot()
     {
-        $setting=\App\Models\Setting::first(); 
-        Paginator::defaultView('my-paginate'); 
-        View::share('recent_posts',\App\Models\Post::orderBy('id','desc')->limit($setting->recent_limit)->get()); 
-        View::share('popular_posts',\App\Models\Post::orderBy('views','desc')->limit($setting->popular_limit)->get());
+      
     }
 }
