db507c06dfa703e05e1cc75b6716af0ee6a83cd5
JeremyPoh <jeremypoh0205@gmail.com>
2022-06-22 16:57:10 +0800

    Project Clone

--- BlogProject/resources/views/auth/login.blade.php
+++ BlogProject/resources/views/auth/login.blade.php
@@ -2,75 +2,76 @@
 @section('title','Login')
 @section('content')
 
-
 <section class="login-register-form">
 
-<div class="acc-container" id="acc-container">
-	<div class="form-container sign-up-container">
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+    <div class="acc-container" id="acc-container">
+        <div class="form-container sign-up-container">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+    
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                     </span>
+                @enderror
+    
+                @error('name')
+                   <span class="invalid-feedback" role="alert">
+                       <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+    
+    
+                <h1 class="acc-title">Create Account</h1>
+                <input id="name" placeholder="Name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    
+                <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                <input id="password"  placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                <input id="password-confirm" placeholder="Confrim Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                <button type ="submit">Sign Up</button>
+            </form>
+        </div>
 
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                 </span>
-            @enderror
-
-            @error('name')
-               <span class="invalid-feedback" role="alert">
-                   <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-
-
-			<h1 class="acc-title">Create Account</h1>
-            <input id="name" placeholder="Name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-			<input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-            <input id="password"  placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-            <input id="password-confirm" placeholder="Confrim Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-			<button type ="submit">Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		 <form method="POST" action="{{ route('login') }}">
-         @csrf
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-			<h1>SIGN IN</h1>
-            <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-           
-            <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-   
-            <div class="form-check">
-            <label for="remember"> Remember Me</label>                                          
-            <input type="checkbox" name="remember" id="remember" 
-                {{ old('remember') ? 'checked' : '' }}>
+        
+        <div class="form-container sign-in-container">
+             <form method="POST" action="{{ route('login') }}">
+             @csrf
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+                <h1>SIGN IN</h1>
+                <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+               
+                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+       
+                <div class="form-check">
+                <label for="remember"> Remember Me</label>                                          
+                <input type="checkbox" name="remember" id="remember" 
+                    {{ old('remember') ? 'checked' : '' }}>
+                </div>
+                
+                @if (Route::has('password.request'))
+                    <a href="{{ route('password.request') }}">Forgot your password?</a>
+                @endif
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
             </div>
-            
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">Forgot your password?</a>
-            @endif
-			<button type="submit">Sign In</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
-</section>
+        </div>
+    </div>
+    </section>
 @endsection
